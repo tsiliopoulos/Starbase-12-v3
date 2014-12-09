9d3803ab934eae9d9666d635cb33e6c1b9c137ce
tsiliopoulos <tsiliopoulos@hotmail.com>
2014-12-09 11:39:24 -0500

    Base Project setup for version 3

---
+++ Starbase12-v3/Scripts/objects/button.ts
@@ -0,0 +1,25 @@
+﻿// Button Object Class
+module objects {
+    export class Button extends objects.GameObject {
+        constructor(x: number, y: number, buttonIDString: string) {
+            super(buttonIDString);
+            this.x = x;
+            this.y = y;
+            this.setButtonListeners();
+        }
+
+        setButtonListeners() {
+            this.cursor = 'pointer';
+            this.on('rollover', this.onButtonOver);
+            this.on('rollout', this.onButtonOut);
+        }
+
+        onButtonOver() {
+            this.alpha = 0.8;
+        }
+
+        onButtonOut() {
+            this.alpha = 1;
+        }
+    }
+} 
